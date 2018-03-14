5b8b19eb6c779effd5ed478e5b04180b0b52a786
yossi katzman <issoyk@gmail.com>
2018-03-14 21:29:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector("#nested .target")
+}
+
+function deepestChild() {
+ return document.querySelector("#grand-node div div div div")
+}
+
+function increaseRankBy(n) {
+ var main = document.getElementsByClassName("ranked-list")
+ for (var i = 0; i < main.length; i++) {
+ main[i].innerHTML = parseInt(main[i].innerHTML =+ n)
+ }
+}
